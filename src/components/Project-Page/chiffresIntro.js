893b6a24cb1f72ce3c4e3b6fdbc801f93b9071a0
Olivier <olivier.atesyan@ieseg.fr>
2020-11-29 19:43:01 +0100

    fix chiffre intro size & add space for thousands

--- src/components/Project-Page/chiffresIntro.js
+++ src/components/Project-Page/chiffresIntro.js
@@ -5,12 +5,19 @@ const ChiffresIntro = ({ chiffres }) => {
   return (
     <Container className="container-home">
       <div className="chiffres-container">
-        {chiffres.map((chiffre) => (
-          <div>
-            <h2>{chiffre.chiffre}</h2>
-            <h2>{chiffre.chiffre_description}</h2>
-          </div>
-        ))}
+        {chiffres.map((chiffre) => {
+          const chiffreEspace = chiffre.chiffre
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
+          return (
+            <div>
+              <h2>{chiffreEspace}</h2>
+              <h2 className="chiffre-description">
+                {chiffre.chiffre_description}
+              </h2>
+            </div>
+          );
+        })}
       </div>
     </Container>
   );
